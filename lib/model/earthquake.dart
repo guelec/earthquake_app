94fa39501df90a87f5f33d8d22de418fa685f3f3
Yiğit <yighitgulec@gmail.com>
2021-03-12 22:51:43 +0300

    Earthquake Class

---
+++ lib/model/earthquake.dart
@@ -0,0 +1,30 @@
+class Earthquake {
+  double mag;
+  double lng;
+  double lat;
+  String loc;
+  double depth;
+  String coordinates;
+  String title;
+  String rev;
+  String timestamp;
+  String date_stamp;
+  String date;
+  String hash;
+  String hash2;
+
+  Earthquake(
+      this.mag,
+      this.lng,
+      this.lat,
+      this.loc,
+      this.depth,
+      this.coordinates,
+      this.date,
+      this.date_stamp,
+      this.hash,
+      this.hash2,
+      this.rev,
+      this.timestamp,
+      this.title);
+}
--- lib/model/network.dart
+++ lib/model/network.dart
@@ -9,7 +9,7 @@ class Network {
   Network(this.url);
 
   Future fetchData() async {
-    Response response = await get(Uri.encodeFull(url));
+    Response response = await get(Uri.parse(url)); // check here
 
     if (response.statusCode == 200) {
       return json.decode(response.body);
